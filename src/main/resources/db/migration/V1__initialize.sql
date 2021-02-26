2d95e04c6cd6fea0ef7e7c45292ae6af01f4b6b6
IMBABOT1 <zlovred12@gmail.com>
2021-02-26 17:22:47 +0300

    pageable

--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -43,7 +43,27 @@ values
 ('Bread16', 37),
 ('Bread17', 38),
 ('Bread18', 39),
+('Bread19', 40),
+('Bread20', 21),
+('Bread2', 22),
+('Bread3', 23),
+('Bread4', 24),
+('Bread5', 25),
+('Bread6', 26),
+('Bread7', 27),
+('Bread8', 28),
+('Bread9', 29),
+('Bread10', 31),
+('Bread11', 32),
+('Bread12', 33),
+('Bread13', 34),
+('Bread14', 35),
+('Bread15', 36),
+('Bread16', 37),
+('Bread17', 38),
+('Bread18', 39),
 ('Bread19', 40);
+
 
 
 insert into orders (customer_id, product_id, current_price)
