690207a18da700358a1f8b1fa3fece72d09d4f71
IMBABOT1 <zlovred12@gmail.com>
2021-02-27 21:01:10 +0300

    temp

--- src/main/java/com/geekbrains/geek/market/services/ProductService.java
+++ src/main/java/com/geekbrains/geek/market/services/ProductService.java
@@ -39,7 +39,7 @@ public class ProductService {
         return productRepository.findAll(spec, PageRequest.of(page, size));
     }
 
-    public Page<Product> edit(Long id, String title, Integer price, int page, int size){
+    public Page<Product> edit(Long id, String title, Integer price){
         Specification<Product> spec = Specification.where(null);
         if (title != null && !title.isBlank()){
            spec = spec.and(ProductSpecifications.titleEquals(title));
@@ -51,10 +51,15 @@ public class ProductService {
             spec = spec.and(ProductSpecifications.priceEquals(price));
        }
 
+        if (spec.equals(id)){
+
+        }
+
+
         Product product = new Product();
         product.setId(id);
         product.setTitle(title);
         product.setPrice(price);
-       return productRepository.save();
+       return productRepository.findAll(spec, )
    }
 }
